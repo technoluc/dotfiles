df15910c9cc06f2279df2f25d001508675864617
TechnoLuc <94373293+technoluc@users.noreply.github.com>
2022-10-25 00:32:35 +0200

    add

---
+++ dotfiles/bootstrap.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env zsh
+
+# Set the colours you can use
+black='\033[0;30m'
+white='\033[0;37m'
+red='\033[0;31m'
+green='\033[0;32m'
+yellow='\033[0;33m'
+blue='\033[0;34m'
+magenta='\033[0;35m'
+cyan='\033[0;36m'
+
+# Resets the style
+reset=`tput sgr0`
+
+echo ""
+cecho "###############################################" $red
+cecho "#                                             #" $red
+cecho "#                                             #" $red
+cecho "#      TechnoLuc's macos bootstrap script     #" $red
+cecho "#                                             #" $red
+cecho "#                                             #" $red
+cecho "###############################################" $red
+echo ""
+
+echo ""
+cecho "Have you read through the script you're about to run and " $red
+cecho "understood that it will make changes to your computer? (y/n)" $red
+read -r response
+if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
+  CONTINUE=true
+fi
+
+if ! $CONTINUE; then
+  # Check if we're continuing and output a message if not
+  cecho "Please go read the script, it only takes a few minutes" $red
+  exit
+fi
+
+# Close any open System Preferences panes, to prevent them from overriding
+# settings we’re about to change
+osascript -e 'tell application "System Preferences" to quit'
+
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until `.macos` has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+./general.sh
+
