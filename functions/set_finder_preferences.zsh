98828ebb15ffc9fb191dff46d78bff2e98f669a0
TechnoLuc <94373293+technoluc@users.noreply.github.com>
2023-10-25 09:59:04 +0200

    updates

--- functions/set_finder_preferences.zsh
+++ functions/set_finder_preferences.zsh
@@ -14,108 +14,86 @@ set_finder_preferences() {
       show_ok "Showing the ~/Library folder."
     fi
 
-    # Show hidden files by default
+    # Other Finder preferences
     if prompt_robot "Show hidden files by default?"; then
       defaults write com.apple.finder AppleShowAllFiles -bool true
-      show_ok "Showing hidden files by default."
+      show_ok "Enabled showing hidden files by default."
     fi
 
-    # Other Finder preferences
-    botq "Show hidden files by default?"
-    read -r response
-    if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
-      defaults write com.apple.finder AppleShowAllFiles -bool true
-    fi
-
-    botq "Show all filename extensions?"
-    read -r response
-    if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
+    if prompt_robot "Show all filename extensions?"; then
       defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+      show_ok "Enabled showing all filename extensions."
     fi
 
-    botq "Show status bar?"
-    read -r response
-    if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
+    if prompt_robot "Show status bar?"; then
       defaults write com.apple.finder ShowStatusBar -bool true
+      show_ok "Enabled showing the status bar."
     fi
 
-    botq "Show path bar?"
-    read -r response
-    if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
+    if prompt_robot "Show path bar?"; then
       defaults write com.apple.finder ShowPathbar -bool true
+      show_ok "Enabled showing the path bar."
     fi
 
-    botq "Keep folders on top when sorting by name?"
-    read -r response
-    if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
+    if prompt_robot "Keep folders on top when sorting by name?"; then
       defaults write com.apple.Finder _FXSortFoldersFirst -bool true
+      show_ok "Folders are kept on top when sorting by name."
     fi
 
-    botq "Allow text selection in Quick Look?"
-    read -r response
-    if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
+    if prompt_robot "Allow text selection in Quick Look?"; then
       defaults write com.apple.finder QLEnableTextSelection -bool TRUE
+      show_ok "Enabled text selection in Quick Look."
     fi
 
-    botq "Disable the warning when changing a file extension?"
-    read -r response
-    if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
+    if prompt_robot "Disable the warning when changing a file extension?"; then
       defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+      show_ok "Disabled the warning when changing a file extension."
     fi
 
-    botq "Calculate all folder sizes?"
-    read -r response
-    if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
+    if prompt_robot "Calculate all folder sizes?"; then
       /usr/libexec/PlistBuddy "$HOME/Library/Preferences/com.apple.finder.plist" -c 'Delete "StandardViewSettings:ExtendedListViewSettings:calculateAllSizes" bool'
       /usr/libexec/PlistBuddy "$HOME/Library/Preferences/com.apple.finder.plist" -c 'Add "StandardViewSettings:ExtendedListViewSettings:calculateAllSizes" bool true'
-
       /usr/libexec/PlistBuddy "$HOME/Library/Preferences/com.apple.finder.plist" -c 'Delete "StandardViewSettings:ExtendedListViewSettings:useRelativeDates" bool'
       /usr/libexec/PlistBuddy "$HOME/Library/Preferences/com.apple.finder.plist" -c 'Add "StandardViewSettings:ExtendedListViewSettings:useRelativeDates" bool false'
-
       /usr/libexec/PlistBuddy "$HOME/Library/Preferences/com.apple.finder.plist" -c 'Delete "StandardViewSettings:ListViewSettings:calculateAllSizes" bool'
       /usr/libexec/PlistBuddy "$HOME/Library/Preferences/com.apple.finder.plist" -c 'Add "StandardViewSettings:ListViewSettings:calculateAllSizes" bool true'
+      show_ok "Enabled calculating all folder sizes."
     fi
 
-    botq "Enabling snap-to-grid for icons on the desktop and in other icon views?"
-    read -r response
-    if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
+    if prompt_robot "Enabling snap-to-grid for icons on the desktop and in other icon views?"; then
       /usr/libexec/PlistBuddy -c "Set :DesktopViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
       /usr/libexec/PlistBuddy -c "Set :FK_StandardViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
       /usr/libexec/PlistBuddy -c "Set :StandardViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
+      show_ok "Enabled snap-to-grid for icons on the desktop and in other icon views."
     fi
 
-    botq "Avoid creating .DS_Store files on network Volumes?"
-    read -r response
-    if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
+    if prompt_robot "Avoid creating .DS_Store files on network Volumes?"; then
       defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+      show_ok "Avoided creating .DS_Store files on network Volumes."
     fi
 
-    botq "Avoid creating .DS_Store files on USB Volumes?"
-    read -r response
-    if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
+    if prompt_robot "Avoid creating .DS_Store files on USB Volumes?"; then
       defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
+      show_ok "Avoided creating .DS_Store files on USB Volumes."
     fi
 
-    botq "Disable disk image verification?"
-    read -r response
-    if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
+    if prompt_robot "Disable disk image verification?"; then
       defaults write com.apple.frameworks.diskimages skip-verify -bool true
       defaults write com.apple.frameworks.diskimages skip-verify-locked -bool true
       defaults write com.apple.frameworks.diskimages skip-verify-remote -bool true
+      show_ok "Disabled disk image verification."
     fi
 
-    botq "Expand save panel by default?"
-    read -r response
-    if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
+    if prompt_robot "Expand save panel by default?"; then
       defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
       defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode2 -bool true
+      show_ok "Expanded save panel by default."
     fi
 
-    botq "Expand print panel by default?"
-    read -r response
-    if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
+    if prompt_robot "Expand print panel by default?"; then
       defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
       defaults write NSGlobalDomain PMPrintingExpandedStateForPrint2 -bool true
+      show_ok "Expanded print panel by default."
     fi
   fi
 }
